7ca50af28ab56c6508937f91fb696266b3c809ce
oner <740778222@qq.com>
2019-04-19 10:41:33 +0800

    ...

---
+++ src/main/java/com/qianfeng/common/CommonException.java
@@ -0,0 +1,23 @@
+package com.qianfeng.common;
+
+import com.qianfeng.utils.JsonUtils;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//@ControllerAdvice和@ResponseBody
+@RestControllerAdvice
+public class CommonException {
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public JsonBean permException(UnauthorizedException e){
+        return JsonUtils.createJsonBean(0, e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public JsonBean commonException(Exception e){
+
+        return JsonUtils.createJsonBean(0, e.getMessage());
+    }
+
+}
